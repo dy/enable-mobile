580c4c6fc67417999fec5184f38463472da3d729
Dmitry <df.creative@gmail.com>
2016-08-21 14:28:16 -0400

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+addMeta({
+	name: 'viewport',
+	content: 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0'
+});
+addMeta({
+	'http-equiv': 'x-ua-compatible',
+	content: 'ie=edge'
+});
+addMeta({
+	charset: 'utf-8'
+});
+
+
+function addMeta (obj) {
+	let meta = document.createElement('meta');
+	for (let name in obj) {
+		meta.setAttribute(name, obj[name]);
+	}
+	document.head.insertBefore(meta, document.head.firstChild);
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# enable-mobile
+
+Include that script to enable mobile view.
+
+```js
+require('enable-mobile');
+```
+
+That will insert the following code:
+
+```
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0"/>
+```
+
+Useful for tests, apps, demo.
+
+Ideas for enhancements are welcomed.
